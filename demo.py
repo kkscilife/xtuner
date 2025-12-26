43c859344ed93289f28549d7088155f6c22a14d1
kkscilife <1658148753@qq.com>
2025-12-26 14:02:49 +0800

    debug

--- demo.py
+++ demo.py
@@ -15,6 +15,7 @@ def embed_image_to_summary(image_path, alt_text="Image"):
         with open(summary_file, 'a') as f:
             f.write(f'\n![{alt_text}](data:image/png;base64,{encoded_string})\n')
             f.write(f'\n![url_way](https://github.com/kkscilife/xtuner/blob/ci/debug/qwen3-sft_comparison.png)\n')
+            f.write(f'\n![githubusercontent](https://raw.githubusercontent.com/kkscilife/xtuner/blob/ci/debug/qwen3-sft_comparison.png)\n')
             f.write(f'\n<div align="left">\n <img src="./qwen3-sft_comparison.png" width="580"/>\n</div>\n')
 
 embed_image_to_summary("./output_simple.png")
